8b0b952cd364a6e2fa854df503b1e91f5a6e9f74
MinKyeong031 <s2019s07@e-mirim.hs.kr>
2021-03-24 12:50:23 +0900

    Initial commit

---
+++ app/src/main/java/kr/hs/mirim/mp3_java_/MainActivity.java
@@ -0,0 +1,14 @@
+package kr.hs.mirim.mp3_java_;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
